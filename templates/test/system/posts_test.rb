1d75dbb523443233c36adb6429b43fe8a0c18256
Lee McAlilly <leemcalilly@gmail.com>
2020-10-31 12:25:15 -0500

    Fix failing tests after updating the Posts layout

--- templates/test/system/posts_test.rb
+++ templates/test/system/posts_test.rb
@@ -9,7 +9,7 @@ class PostsTest < ApplicationSystemTestCase
 
   test "visiting the index should be accessible to all" do
     visit posts_path
-    assert_selector "h1", text: "Posts"
+    assert_selector "h1", text: "Blog"
   end
 
   test "viewing a post should be accessible to all" do
@@ -21,56 +21,59 @@ class PostsTest < ApplicationSystemTestCase
 
   test "non-admins should not be able to create, edit, or delete" do
     visit posts_path(as: @non_admin)
-    click_on "New Post"
+    assert_no_selector "a", text: "New Post"
+
+    visit new_post_path(as: @non_admin)
     within(".flash") do
       assert_text "You cannot perform this action."
     end
 
     visit posts_path(as: @non_admin)
-    click_on "Edit", match: :first
+    click_on @post.title
+    assert_no_selector "a", text: "Edit"
+    visit edit_post_path(@post)
     within(".flash") do
       assert_text "You cannot perform this action."
     end
 
     visit posts_path(as: @non_admin)
-    click_on "Show", match: :first
-    assert_selector "a", text: "Delete", count: 0
+    click_on @post.title
+    assert_no_selector "a", text: "Delete"
   end
 
   test "creating a Post" do
     visit posts_path(as: @admin)
     click_on "New Post"
-    fill_in "Title", with: "Another Unique Title"
-    find(:css, ".trix-content").set("This is the body of the post.")
-    attach_file("post[featured_image]", "#{Rails.root.join("test/fixtures/files/example-featured-image.jpg")}")
-    fill_in "Description", with: "A new description for a new post."
-    click_button "Create Post"
+    within("form") do
+      fill_in "Title", with: "Another Unique Title"
+      find(:css, "trix-editor").set("This is the body of the post.")
+      attach_file("post[featured_image]", "#{Rails.root.join("test/fixtures/files/example-featured-image.jpg")}")
+      fill_in "Description", with: "A new description for a new post."
+      click_button "Save"
+    end
     sleep 0.5
 
     within(".flash") do
       assert_text "Post was successfully created"
     end
-
-    click_on "Back"
   end
 
   test "updating a Post" do
     visit posts_path(as: @admin)
-    click_on "Edit", match: :first
-
-    fill_in "Title", with: @post.title
-    click_button "Update Post"
+    click_on @post.title
+    click_on "Edit"
+    fill_in "Title", with: "A new unique title"
+    click_button "Save"
 
     within(".flash") do
       assert_text "Post was successfully updated"
     end
-    click_on "Back"
   end
 
-  test "destroying a Post" do
-    visit posts_path(as: @admin)
+  test "deleting a Post" do
+    visit post_path(@post, as: @admin)
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on "Delete", match: :first
     end
 
     within(".flash") do
