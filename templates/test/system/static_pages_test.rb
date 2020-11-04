cbdd8f9a24e40c3bd2682e46016edfb3e3aa8180
Lee McAlilly <leemcalilly@gmail.com>
2020-11-04 14:23:35 -0600

    Add example for how to include a page title in a view

--- templates/test/system/static_pages_test.rb
+++ templates/test/system/static_pages_test.rb
@@ -4,6 +4,6 @@ class StaticPagesTest < ApplicationSystemTestCase
 
   test "Home page should work and have the right content" do
     visit root_path
-    assert_title "It's a New Spark Website! • Designed With Spark"
+    assert_title "Spark CMS • Designed With Spark"
   end
 end
