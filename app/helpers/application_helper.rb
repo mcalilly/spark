b2ebeedf3d81f6637cd78a07f08d836e3004aaf1
Lee McAlilly <leemc@hey.com>
2022-01-21 23:04:11 -0600

    Fix error application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,33 +37,33 @@ module ApplicationHelper
     end
   end
 
-  settings = Setting.last
+  @settings = Setting.last
 
   def site_title
-    site_title = settings.site_title
+    site_title = @settings.site_title
   end
 
   def site_tagline
-    site_tagline = settings.site_tagline
+    site_tagline = @settings.site_tagline
   end
 
   def site_description
-    site_description = settings.site_description
+    site_description = @settings.site_description
   end
 
   def site_email
-    site_email = settings.email
+    site_email = @settings.email
   end
 
   def facebook_handle
-    facebook_handle = settings.twitter_handle
+    facebook_handle = @settings.twitter_handle
   end
 
   def twitter_handle
-    twitter_handle = settings.twitter_handle
+    twitter_handle = @settings.twitter_handle
   end
 
   def instagram_handle
-    instagram_handle = settings.instagram_handle
+    instagram_handle = @settings.instagram_handle
   end
 end
