30188191bb85871552c34bc318e201d68079803b
Lee McAlilly <leemc@hey.com>
2022-01-28 19:52:02 -0600

    this is breaking the file upload test that uses active storage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
