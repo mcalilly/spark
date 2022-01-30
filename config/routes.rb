4982eddef11692522376315dcb876c209c2816c7
Lee McAlilly <leemc@hey.com>
2022-01-29 18:30:02 -0600

    Add a test for the /admin login route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get "/sign-in", to: "devise/sessions#new", as: "sign_in"
+    get "/admin", to: "devise/sessions#new"
     get "/sign-up", to: "devise/registrations#new", as: "sign_up"
     get "/logout", to: "devise/sessions#destroy", as: "log_out"
   end
@@ -14,9 +15,8 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about", as: "about"
   get "/contact-us", to: "static_pages#contact", as: "contact"
 
-  # Settings
+  # Admin & Settings
   namespace :admin do
     resources :settings
   end
-
 end
--- test/system/user_sign_in_test.rb
+++ test/system/user_sign_in_test.rb
@@ -11,6 +11,11 @@ class UserSignInTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Sign in"
   end
 
+  test "/admin should forward you to the sign in page" do
+    visit "/admin"
+    assert_selector "h1", text: "Sign in"
+  end
+
   test "Invalid sign in information" do
     visit sign_in_path
     assert_current_path sign_in_path
@@ -23,4 +28,27 @@ class UserSignInTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Sign in"
   end
 
+  test "Valid sign in information" do
+    visit sign_in_path
+    assert_current_path sign_in_path
+    within "main" do
+      fill_in("Email address", with: "admin@example.com")
+      fill_in("Password", with: "password")
+      click_button "Sign in"
+    end
+    within ".sidebar-desktop" do
+      click_on "Log out"
+    end
+    assert_current_path root_path
+    visit "/admin"
+    within "main" do
+      fill_in("Email address", with: "admin@example.com")
+      fill_in("Password", with: "password")
+      click_button "Sign in"
+    end
+    within ".sidebar-desktop" do
+      click_on "Log out"
+    end
+    assert_current_path root_path
+  end
 end
