00dd1175dd08ce515de64d974c9378eb5df94ce2
Lee McAlilly <leemc@hey.com>
2022-03-01 14:03:28 -0600

    Update Pundit include to current version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  include Pundit
+  include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   layout :determine_layout
